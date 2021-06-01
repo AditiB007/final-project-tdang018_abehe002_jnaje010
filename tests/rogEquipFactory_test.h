f5625386444cbf7dc294bbc8efd6e4db2790baa1
tdang93 <aznnoodle101@hotmail.com>
2021-06-01 13:44:29 -0700

    All Factory tests working, passing

--- tests/rogEquipFactory_test.h
+++ tests/rogEquipFactory_test.h
@@ -5,6 +5,8 @@
 
 #include "../abstractFactory/rogEquipFactory.h"
 #include "../player/rogue.hpp"
+#include "../abstractFactory/magEquipFactory.h"
+#include "../abstractFactory/warEquipFactory.h"
 
 TEST(RogFactTest, Init) {
 	RogEquipFactory* test = new RogEquipFactory();
@@ -16,6 +18,35 @@ TEST(RogFactTest, EquipRogue) {
 	RogEquipFactory* fact = new RogEquipFactory();
 	Rogue* rogue = new Rogue("Rogue", 2, 3, 4, 5);
 	fact->makeEquipment(rogue);
+
+	int attack = rogue->weapon->getAttack();
+	EXPECT_EQ(attack, 2);
+
+	delete fact;
+	delete rogue;
+}
+
+TEST(RogFactTest, RogSwapEquip) {
+	IEquipFactory* fact = new RogEquipFactory();
+	IPlayer* rogue = new Rogue("Rogue", 2, 3, 4, 5);
+	fact->makeEquipment(rogue);
+
+        int attack = rogue->weapon->getAttack();
+	EXPECT_EQ(attack, 2);
+
+	delete fact;
+	fact = new MagEquipFactory();
+	fact->makeEquipment(rogue);
+	
+	attack = rogue->weapon->getAttack();
+	EXPECT_EQ(attack, 1);
+
+	delete fact;
+	fact = new WarEquipFactory();
+	fact->makeEquipment(rogue);
+
+	attack = rogue->weapon->getAttack();
+	EXPECT_EQ(attack, 3);
 
 	delete fact;
 	delete rogue;
